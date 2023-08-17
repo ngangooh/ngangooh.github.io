934ba45a32354afb13cedc2b78034f0fef9c66ec
ngangooh <ngangooh@gmail.com>
2023-08-17 09:19:24 +0700

    resize card into 2 column

--- data.js
+++ data.js
@@ -55,13 +55,14 @@ for (let i in data.menu) {
 
 	for (let j in x.daftar) {
 		let y = x.daftar[j];
-		htm += '<div class="col-sm-4 p-1">';
+		htm += '<div class="col-sm-4 col-6 p-1">';
 		htm += '<div class="card bg-warning-subtle">';
 		htm += '<img src="img/' + y.gambar + '" class="card-img-top p-1" alt="' + y.nama + '" />';
 		htm += '<div class="card-body">';
-		htm += '<h5 class="card-title">' + y.nama + ' <span class="badge text-bg-primary float-end">' + y.harga + ' K</span>';
+		htm += '<h5 class="card-title">' + y.nama + '</h5>';
+		htm += '<p><span class="badge text-bg-primary float-start">' + y.harga + ' K</span>';
 		if (y.laris) htm += ' <span class="badge text-bg-success float-end me-1">Terlaris</span>';
-		htm += '</h5>';
+		htm += '</p>';
 		htm += '</div></div></div>';
 	}
 	htm += '</section>';
